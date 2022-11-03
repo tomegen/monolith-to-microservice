423ace16b9cf354199aa8edebdca89277bdaa078
Thomas Egen (DE-722) <Thomas.Egen@bmw.de>
2022-11-03 09:21:58 +0100

    Divide monolith into microservice

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,15 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=tom
+export POSTGRES_PASSWORD=hallihallo
+export POSTGRES_HOST=tomdatabase.cvqp9a8iqbnm.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::test-472916243171-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+export HTTP_PROXY=
+export HTTPS_PROXY=
+export ALL_PROXY=
+
